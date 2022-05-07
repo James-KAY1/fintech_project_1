18db30945f4f9a2521443b93d0cea2b7100a8b5d
Ryan Anderson <98444459+Ryanderson94@users.noreply.github.com>
2022-05-06 21:24:29 -0400

    Updated helper function to include API object
    Updated helper function to include API object

--- utils/helper.py
+++ utils/helper.py
@@ -8,17 +8,19 @@ import os
 # Function to get Alpacas trading account info
 def get_alpacas_info():
     load_dotenv('api.env')
-    alpaca_api_key = os.getenv('APCA_API_KEY_ID')
-    alpaca_secret_key = os.getenv('APCA_API_SECRET_KEY')
+    alpaca_api_key = os.getenv('ALPACA_API_KEY')
+    alpaca_secret_key = os.getenv('ALPACA_SECRET_KEY')
+    base_url = 'https://paper-api.alpaca.markets'
 
     # Create alpacas API object
     api = tradeapi.REST(
         alpaca_api_key,
         alpaca_secret_key,
+        base_url,
         api_version='v2'
     )
 
     # Assign account information to variable
     account = api.get_account()
 
-    return account
+    return account, api
